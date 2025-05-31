27fa0bf2196c1ddb7d07bc7ecc68ca8b596b2155
Piotr Daniel <p.daniel.london@gmail.com>
2025-06-01 00:26:33 +0100

    docs: add typehinting and docstring to read_snippet fun

--- app/db.py
+++ app/db.py
@@ -5,6 +5,7 @@ DATA_PATH = os.path.join(os.path.dirname(__file__), "snippets.json")
 
 
 def get_snippets_by_topic(topic: str):
+    """Retrieves JSON object for given topic"""
     with open(DATA_PATH, "r") as f:
         data = json.load(f)
     return data.get(topic.lower())
--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ app = FastAPI(title="NibblePy API", version="1.0")
 
 @app.get("/snippet", response_model=SnippetResponse)
 def read_snippet(topic: str = Query(..., description="Topic of the snippet")):
+    """Fetch code snippet for the given topic"""
     snippet = get_snippets_by_topic(topic)
     if snippet:
         return snippet
